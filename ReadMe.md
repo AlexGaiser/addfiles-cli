4ce11b0c4fb980509bd330919eeccc60ff52f5fb
AlexGaiser <alexcgaiser@gmail.com>
2021-03-31 14:04:29 -0400

    adds more robust flag management

--- ReadMe.md
+++ ReadMe.md
@@ -7,9 +7,31 @@ Will create a __tests__ file if not already present.
 
 ## How To Use
 
-in the command line run the command `addfile`
+in the command line run the command `simplemkfile`
 
-then pass after the `-n` or `--name` flag the name of the file you want to create
-then pass after the `-t` or `--type` flag the file extension. 
+### flags
+`-n` or `--name` flag is used to designate the name of the file you want to create  
 
-The CLI tool will create a file with the name you give it (along with some boilerplate code) as well as a test file that imports your file.
+`-t` or `--type` flag designates the file extension (js by default)
+
+`-tf` or `--testfile` flag enables or disables creation of the test file and directory (true by default)  
+
+As a shortcut, you can pass `[name] [type] [testfile]` as the first in this order to pass the arguments without flags.  
+
+The CLI tool will create a file with the name you give it (along with some boilerplate code) as well as a test (jest) file that imports your file.
+
+
+## Example:
+
+create a typescript file and corresponding test file
+```bash
+$ simplemkfile testfile ts
+
+```
+
+create  typescript file and corresponding test file using flags
+```bash
+$ simplemkfile --name testfile --type ts
+
+
+```
--- cli.js
+++ cli.js
@@ -1,52 +1,95 @@
 #!/usr/bin/env node
-
 const fs = require("fs");
 const path = require("path");
 const args = process.argv.slice(2);
-const type =
-  args[
-    args.findIndex((a) => {
-      return a === "--type" || a === "-t";
-    }) + 1
-  ];
-const name =
-  args[0][0] !== "-"
-    ? args[0]
-    : args[
-        args.findIndex((a) => {
-          return a === "--name" || a === "-n";
-        }) + 1
-      ];
-console.log("args", args);
+
+if (args.length < 1) {
+  console.log("Error: please provide a name for the file to be created");
+  process.exit(0);
+}
+
+const defaults = {
+  type: "js",
+  testfile: true,
+  dir: path.join(process.cwd()),
+  tfdir: path.join(process.cwd(), "__tests__"),
+};
+
+const isFlag = (str) => {
+  return str[0] === "-";
+};
+
+const getArg = (name, shortname, noFlagIndex) => {
+  if (noFlagIndex !== undefined && args[noFlagIndex] === undefined)
+    return undefined;
+
+  const flagIndex = args.findIndex((a) => {
+    return a === name || a === shortname;
+  });
+  const flagValue = args[flagIndex + 1];
+
+  if (flagIndex > 0 && flagValue && !isFlag(flagValue)) {
+    return flagValue;
+  }
+
+  if (args[noFlagIndex] !== undefined && !isFlag(args[noFlagIndex])) {
+    return args[noFlagIndex];
+  }
+
+  return undefined;
+};
+
+const name = getArg("--name", "-t", 0);
+const type = getArg("--type", "-t", 1) || defaults.type;
+const dir = getArg("--dir", "-d") || defaults.dir;
+const tfdir = getArg("--tfdir", "-tf") || defaults.tfdir;
+
 const testfileName = `${name}.test.${type}`;
 const filename = `${name}.${type}`;
 
-const destinationFile = path.join(process.cwd(), filename);
-const destinTestFile = path.join(process.cwd(), "__tests__", testfileName);
-
-if (!fs.existsSync("__tests__")) {
-  fs.mkdirSync("__tests__");
-}
-
-fs.writeFileSync(
-  destinationFile,
-  `
-  const ${name} = () => {
-
+const destinationFile = path.join(
+  dir[0] === "." ? "." : process.cwd(),
+  dir,
+  filename
+);
+const destinTestFile = path.join(
+  tfdir[0] === "." ? "." : process.cwd(),
+  tfdir,
+  testfileName
+);
+try {
+  if (!fs.existsSync(tfdir)) {
+    fs.mkdirSync(tfdir, { recursive: true });
+  }
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
   }
 
-  export default ${name}
-`
-);
-fs.writeFileSync(
-  destinTestFile,
-  `
-  import ${name} from "../${name}";
+  fs.writeFileSync(
+    destinationFile,
+    `
+const ${name} = () => {
 
-  describe('testing ${name}', ()=>{
-    
+}
 
-  })
+export default ${name}
+`,
+    { recursive: true }
+  );
+  fs.writeFileSync(
+    destinTestFile,
+    `
+import ${name} from "${path
+      .relative(destinTestFile, destinationFile)
+      .replace(/\.[^/.]+$/, "")}";
 
-`
-);
+describe('testing ${name}', ()=>{
+
+})
+  `,
+    { recursive: true }
+  );
+} catch (e) {
+  console.log(e.message);
+  process.exit(0);
+}
